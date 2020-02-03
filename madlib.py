34a4cf39ee2bfa0182a082b72df2e03a41af655c
MattForreal <matthew.farrell3@marist.edu>
2020-02-03 18:08:05 -0500

    Solution to Mad Libs excercise.

---
+++ madlib.py
@@ -0,0 +1,8 @@
+adj1 = input('Type an adjective: ')
+noun1 = input('Type a plural noun: ')
+noun2 = input('Type a singular noun: ')
+adj2 = input('Type another adjective: ')
+
+print(adj1, noun1, noun2, adj2)
+
+print('\n\nRoses are {0}\n{1} are blue\n{2} is {3}\nand so are you'.format(adj1, noun1, noun2,adj2))
