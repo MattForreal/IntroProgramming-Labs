f5ec21c3d6eba58fbdfba04b07423cd93b6f6ef1
Matt Farrell <matthew.farrell3@marist.edu>
2020-02-17 17:49:04 -0500

    More usability improvements to the guessing game

--- guessing-game.py
+++ guessing-game.py
@@ -11,8 +11,19 @@ def main():
         guess = input("Guess what animal I am thinking of")
         if guess.lower() == answer:
             zero += 1
+            antelopeScan = 0
+            while antelopeScan == 0:
+                response = input("Do you like this animal? Y/N: ")
+                if response[0].lower() == "y":
+                    print("Nice, it's a pretty sweet animal")
+                    antelopeScan+=1
+                elif response[0].lower() == "n":
+                    print("Yeah, they're pretty overrated")
+                    antelopeScan += 1
+                else:
+                    print("PLEASE ANSWER THE QUESTION")
             print("Correct answer! Great job!")
-        elif guess.lower() == "quit":
+        elif guess[0].lower() == "q":
             zero += 1
         else:
             print("Wrong answer, please guess again")
