85954f91277a1e1407224c96f695022aa9cb7fa5
Matt Farrell <matthew.farrell3@marist.edu>
2020-02-24 18:21:35 -0500

    Final commit for credentials lab. note: changed the name to refractor.py for second commit which I shouldn't have done but work for second commit can be found under that file in repository.

--- credentials.py
+++ credentials.py
@@ -3,19 +3,33 @@
 # Author: Matt Farrell
 # Created: 2020-02-24
 
-def main():
-    # get user's first and last names
+def name():
     first = input("Enter your first name: ")
     last = input("Enter your last name: ")
+    return first, last
+
+def username():
+    # get user's first and last names
     # TODO modify this to generate a Marist-style username
+    first, last = name()
     uname = first + "." + last
-    # ask user to create a new password
+    print("Here is your Marist-style username.", uname.lower())
+    print("Your Marist-style email will be:", uname.lower(),"@marist.edu")
+
+def pwstrength():
     passwd = input("Create a new password: ")
     # TODO modify this to ensure the password has at least 8 characters
     if len(passwd) < 8:
         print("My bad dawg, needs to be at least 8 characters.")
         passwd = input("Create a new password: ")
-    print("The force is strong in this one…")
-    print("Account configured. Your new email address is",
-    uname + "@marist.edu")
-main()
+    if (passwd.isupper() or passwd.islower()):
+        print("Password would be much stronger with upper and lower case don't you think?")
+        passwd = input("Create a new password: ")
+    print("Here is your new password.", passwd)
+
+def password():
+    # ask user to create a new password
+    username()
+    pwstrength()
+
+password()
