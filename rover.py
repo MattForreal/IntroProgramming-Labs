e67b271e9faf2d1f840f11bb93dd0e42eecb0566
Matt Farrell <matthew.farrell3@marist.edu>
2020-04-18 21:54:05 -0400

    Solution to Curiosity rover exercise.

--- rover.py
+++ rover.py
@@ -1,3 +1,11 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+# Program to calculate how long it takes a photo from Curiosity to reach Nasa
+# Speed of light = 186,000 miles per second
+# Mars distance from earth = 34,000,000 miles
+
+s = 186000
+m = 34000000
+
+t = int(m/s)
+
+print("It would take roughly " + str(t) + " seconds for a photo to reach",
+      "NASA from Mars at its closest orbit to Earth.")
