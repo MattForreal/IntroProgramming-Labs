15cc470e66c86475d4faa57aded799fbcc0e195a
Matt Farrell <matthew.farrell3@marist.edu>
2020-04-18 22:09:15 -0400

    Solution to evens loop exercise.

--- evens.py
+++ evens.py
@@ -1,7 +1,15 @@
-for i in range(10):
- print(i)
- 
-for i in range(2,20,2):
-    print(i)
+# Program designed to print out all the even integers from 2 through 20
 
-    
+print()
+print("Here is a list of the integers from 0 through 9.")
+print()
+for i in range(10):
+    print(i)
+print()
+print("Here is a list of all of the even integers from 2 through 20.")
+print()
+for i in range(2,22,2):
+    print(i)
+print()
+print("Ta-Da.")
+print()
