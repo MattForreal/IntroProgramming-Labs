6cbb20c186d7534a10bffa8c25a11be5bdfb8a93
Matt Farrell <matthew.farrell3@marist.edu>
2020-04-18 22:21:44 -0400

    Solution to Madlibs exercise.

--- madlib.py
+++ madlib.py
@@ -1,8 +1,14 @@
+# Program to prompt the user to enter four different words and then use the user's input to construct an amusing sentence
+
+print()
 adj1 = input('Type an adjective: ')
+print()
 noun1 = input('Type a plural noun: ')
+print()
 noun2 = input('Type a singular noun: ')
+print()
 adj2 = input('Type another adjective: ')
-
-print(adj1, noun1, noun2, adj2)
-
-print('\n\nRoses are {0}\n{1} are blue\n{2} is {3}\nand so are you'.format(adj1, noun1, noun2,adj2))
+print()
+print()
+print(     "Roses are " + adj1 + ", " + noun2 + " are blue. ")
+print(      noun1 + " is " + adj2 + ", and so are you.")
